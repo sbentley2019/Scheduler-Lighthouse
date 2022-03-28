57dfe56d051cea071f5369f32d4bb026ce86b503
Stewart Bentley <sbentley000@gmail.com>
2022-03-28 17:30:48 -0400

    built button componenet and added button scss

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,16 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-  return <></>;
+  const buttonClass = `button ${props.confirm && "button--confirm"} ${
+    props.danger && "button--danger"
+  }`;
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
