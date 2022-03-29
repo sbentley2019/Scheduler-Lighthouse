55dd769242e584a96034b6e5af7ac4480489276c
Stewart Bentley <sbentley000@gmail.com>
2022-03-29 13:12:32 -0400

    added styling and formatSpot function for DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,14 +1,20 @@
 import React from "react";
 import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  let dayClass = classNames({ "day-list__item--full": !props.spots });
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--full": !props.spots,
+    "day-list__item--selected": props.selected,
+  });
+
+  const formatSpot = function (num) {
+    return `${num || "no"} spot${num === 1 ? "" : "s"} remaining`;
+  };
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">
-        {props.spots || "no"} spot{props.spots === 1 ? "" : "s"} remaining
-      </h3>
+      <h3 className="text--light">{formatSpot(props.spots)}</h3>
     </li>
   );
 }
