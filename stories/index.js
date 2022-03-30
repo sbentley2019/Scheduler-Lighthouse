48423e81dc6acf08015da8bf8a9ca38b0a38b3a6
Stewart Bentley <sbentley000@gmail.com>
2022-03-30 17:07:11 -0400

    created form component for Appointment

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -159,4 +160,20 @@ storiesOf("Appointment", module)
   .add("Status", () => <Status message="Deleting" />)
   .add("Error", () => (
     <Error message="Could not delete" onClose={action("onClose")} />
+  ))
+  .add("Edit", () => (
+    <Form
+      student="Marcia Rose"
+      interviewer={2}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
   ));
