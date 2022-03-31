97dcf5004bd1b311be8336cdca59cda81852a511
Stewart Bentley <sbentley000@gmail.com>
2022-03-31 14:04:39 -0400

    display appointments in Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      {props.time && <p>appointment at {props.time}</p>}
-      {!props.time && <p>no appointments</p>}
+      <Header time={props.time} />
+      {props.interview ? <Show {...props.interview} /> : <Empty />}
     </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -176,4 +176,20 @@ storiesOf("Appointment", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </>
+  ))
+  .add("Appointment Booked", () => (
+    <>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </>
   ));
