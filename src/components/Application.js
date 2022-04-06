555f9b86416529c162ad88d803e05356ced905aa
Stewart Bentley <sbentley000@gmail.com>
2022-04-06 15:45:56 -0400

    used axios to get days data from server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,24 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from "axios";
 const appointments = {
   1: {
     id: 1,
@@ -60,6 +44,14 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then((res) => {
+      setDays([...res.data]);
+    });
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
