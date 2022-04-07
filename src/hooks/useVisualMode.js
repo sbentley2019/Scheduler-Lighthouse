1f5c80d3f74c2b30d5fac830595cbb38d8261b82
Stewart Bentley <sbentley000@gmail.com>
2022-04-07 16:22:46 -0400

    created custom hook useVisualMode and tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    setHistory((prev) => [
+      ...(replace ? prev.slice(0, history.length - 1) : prev),
+      newMode,
+    ]);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      const historyEnd = history.length - 2;
+      const newMode = history[historyEnd];
+      setHistory((prev) => [...prev.slice(0, historyEnd)]);
+      transition(newMode);
+    }
+  }
+  return { mode, transition, back };
+}
