31fe8496c880075737cc35feb864f438e6a96a6f
Stewart Bentley <sbentley000@gmail.com>
2022-04-11 12:43:42 -0400

    create getInterviewersForDay function and tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,14 +1,19 @@
 export function getAppointmentsForDay(state, day) {
+  if (state.days.length === 0) return [];
   const ids = state.days.find((e) => e.name === day);
   if (!ids || !ids.appointments.length) return [];
-  const appointments = ids.appointments.map((e) => state.appointments[e]);
-  console.log("appointments: ", appointments);
-  return appointments;
+  return ids.appointments.map((e) => state.appointments[e]);
 }
 
 export function getInterview(state, interview) {
   if (!interview) return null;
   const interviewer = state.interviewers[interview.interviewer];
-  const newInt = { ...interview, interviewer };
-  return newInt;
+  return { ...interview, interviewer };
+}
+
+export function getInterviewersForDay(state, day) {
+  if (state.days.length === 0) return [];
+  const ids = state.days.find((e) => e.name === day);
+  if (!ids || !ids.interviewers.length) return [];
+  return ids.interviewers.map((e) => state.interviewers[e]);
 }
