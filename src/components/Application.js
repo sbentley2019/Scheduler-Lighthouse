4dc8e76b6d057b5f2038dd29daa159ab15d7afc2
Stewart Bentley <sbentley000@gmail.com>
2022-04-11 12:46:40 -0400

    implement getInterviewersForDay into the Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,11 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import axios from "axios";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -40,7 +44,7 @@ export default function Application(props) {
           key={appointment.id}
           {...appointment}
           interview={interview}
-          interviewers={Object.values(state.interviewers)}
+          interviewers={getInterviewersForDay(state, state.day)}
         />
       );
     }
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,14 +10,6 @@ import Status from "components/Appointment/Status";
 
 import useVisualMode from "hooks/useVisualMode";
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
-];
-
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
@@ -38,7 +30,7 @@ export default function Appointment(props) {
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE && (
         <Form
-          interviewers={interviewers}
+          interviewers={props.interviewers}
           onSave={() => console.log("onSave")}
           onCancel={() => back()}
         />
